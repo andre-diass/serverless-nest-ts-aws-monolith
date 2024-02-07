f8b2cbd43f62e39a1147ff55e91155726e3f38b9
ANDRÉ DIAS S <adsf@poli.br>
2024-02-07 14:27:14 -0300

    feat: implem. RepositoriesFactory

---
+++ src/infra/RepositoriesFactory.ts
@@ -0,0 +1,14 @@
+import { ProductWriteRepo } from './repositories/ProductRepositoryDB';
+
+export class RepositoriesFactory {
+  async user_repository() {
+    // return new UserRepositoryDB();
+  }
+
+  async product_repository() {
+    return {
+      write: new ProductWriteRepo(),
+      //   read: new ProductReadRepo(),
+    };
+  }
+}
