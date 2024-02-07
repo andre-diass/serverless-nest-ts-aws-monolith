b0a103c033970ca8ca155568a2bff821a9c63e1c
ANDRÉ DIAS S <adsf@poli.br>
2024-02-07 14:25:37 -0300

    feat: impl. Product entity

---
+++ src/application/domain/product/Product.ts
@@ -0,0 +1,31 @@
+import { OwnedData } from '../contracts';
+import { ObjectId } from 'mongodb';
+
+export class Product implements OwnedData {
+  constructor(
+    public id: string,
+    public user_id: string,
+    public description: string,
+    public price: number,
+    public category: string,
+    public created_at: Date,
+  ) {}
+
+  static new(
+    user: any,
+    description: string,
+    price: number,
+    category: string,
+  ) {
+    const new_id = new ObjectId().toString();
+    const created_at = new Date();
+    return new Product(
+      new_id,
+      user.id,
+      description,
+      price,
+      category,
+      created_at,
+    );
+  }
+}
