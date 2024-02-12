7e02d2cb63a4c9cfca886022b02eeaad281e6a78
ANDRÉ DIAS S <adsf@poli.br>
2024-02-11 21:16:54 -0300

    feat: implem. creat product user case

---
+++ src/application/usecases/product/CreateProduct.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod';
+import { Product } from '../../domain/product/Product';
+import { Command } from '../../Command';
+
+const input_schema = z.object({
+  name: z.string().min(1),
+  description: z.string().min(1),
+  price: z.number().min(0),
+  //   userId: z.string(),
+  category: z.string().min(1),
+});
+
+type Input = z.infer<typeof input_schema>;
+type Output = Product;
+
+export class CreateProduct extends Command {
+  async execute(auth_data: any, input: Input): Promise<any> {
+    const { name, description, price, category } =
+      await input_schema.parseAsync(input);
+    const new_product = Product.new(
+      'user',
+      description,
+      price,
+      category,
+      name,
+    );
+    const { write } = await this.repositories_factory.product_repository();
+    await write.save(new_product);
+    return new_product;
+  }
+}
