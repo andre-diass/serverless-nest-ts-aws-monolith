f0d13d84ccaa593ef00d6678d996a4f883234491
ANDRÉ DIAS S <adsf@poli.br>
2024-02-11 21:15:59 -0300

    feat: add poist method to product entity

--- src/nest/products/products.controller.ts
+++ src/nest/products/products.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get, HttpCode } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode } from '@nestjs/common';
+import { CreateProduct } from '../../application/usecases/product/CreateProduct';
+import { RepositoriesFactory } from '../../infra/factories/RepositoriesFactory';
+import { catchError } from 'rxjs';
 
 @Controller('products')
 export class ProductsController {
   @Get()
   @HttpCode(200)
-  public get_user() {
+  public get_user(@Body() body: any) {
+    const create_product = new CreateProduct(new RepositoriesFactory());
+    const product = create_product.execute('authData', {
+      name: body.name,
+      category: body.category,
+      description: body.description,
+      price: body.price,
+    });
+
     return 'product created';
   }
 }
