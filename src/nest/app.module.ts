e6aa4da1a4f4abf41940e71a5e1fac53af54dca2
ANDRÉ DIAS S <adsf@poli.br>
2024-02-13 10:12:13 -0300

    fix/debug: create new folder on relative path of 'src', so that typescript will compile to the right path

--- src/nest/app.module.ts
+++ src/nest/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
-import { HelloController } from './hello/hello.controller';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [ProductsModule],
-  controllers: [HelloController],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
