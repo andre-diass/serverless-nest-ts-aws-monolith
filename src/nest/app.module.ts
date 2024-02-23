390a3adc554cda1c6e055c6ee54e2e7eb844e3fc
ANDRÉ DIAS S <adsf@poli.br>
2024-02-23 15:05:55 -0300

    feat: create users and auth modules

--- src/nest/app.module.ts
+++ src/nest/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, AuthModule, UsersModule],
   controllers: [],
   providers: [AppService],
 })
--- src/nest/auth/auth.guard.ts
+++ src/nest/auth/auth.guard.ts
@@ -8,6 +8,7 @@ export class AuthGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
+
     return true;
   }
 }
