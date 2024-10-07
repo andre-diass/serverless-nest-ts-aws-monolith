25cb00f5baba31cc86ac3bced60a3fbee1ee3bb5
andre-diass <adsf@poli.br>
2024-10-07 16:40:02 -0300

    fix: payload should be converted to string

--- src/application/usecases/location/StoreLocation.ts
+++ src/application/usecases/location/StoreLocation.ts
@@ -6,11 +6,13 @@ export class StoreLocation extends Command {
   async execute(paylod: any): Promise<LocationsRecord> {
     // extract coordinates from payload
     const mocked_coordinates = { lat: 80.0, long: 100 };
+    console.log(paylod);
+
+    const coordinates = this.extract_coordinates_from_payload(paylod);
+    console.log(coordinates);
 
     // extract IMEI
     const mocked_imei = 1111111;
-
-    // validate if I shouldn't just use the static method add_location instead of instacing a new document
 
     const geo_point = GeoPoint.new(
       mocked_coordinates.lat,
@@ -22,5 +24,23 @@ export class StoreLocation extends Command {
     const { write } = this.repositories_factory.locations_repository();
     write.save(locations_record);
     return locations_record;
+  }
+
+  extract_coordinates_from_payload(payload: string): {
+    lat: number;
+    long: number;
+  } {
+    const payload_str =
+      typeof payload === 'string' ? payload : String(payload);
+    const regex = /CGNSINF: [^,]*,[^,]*,[^,]*,([^,]+),([^,]+)/;
+    const match = payload_str.match(regex);
+
+    if (match && match.length >= 3) {
+      const lat = parseFloat(match[1]);
+      const long = parseFloat(match[2]);
+      return { lat, long };
+    }
+
+    return { lat: 100, long: 100 };
   }
 }
